af04a84d733ca46ece0c70ab77493add04eeb884
计恩良 <454309007@qq.com>
2019-01-20 14:45:05 +0800

    1
    1

--- QLHomeModel/QLHomeViewController.m
+++ QLHomeModel/QLHomeViewController.m
@@ -62,7 +62,7 @@
     RETableViewSection *section0 = [RETableViewSection section];
     
     QLHomeBannerItem *it = [[QLHomeBannerItem alloc] init];
-    it.datas = [NSArray arrayWithObjects:@"",@"",@"",@"", nil];
+    it.datas = [NSArray arrayWithObjects:@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547976453468&di=0730fb64aadc80b0f2490a430f51aebb&imgtype=0&src=http%3A%2F%2Fimg5.duitang.com%2Fuploads%2Fitem%2F201105%2F31%2F20110531094303_d5JZB.jpg",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547976453465&di=8d62405b2626eb79a8b3ca40ae5bbfe6&imgtype=0&src=http%3A%2F%2Fs16.sinaimg.cn%2Fmw690%2F006bYgeozy7pHERVUC3df%26690",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547976453464&di=0b41a9f182baf71c4993b63874cbe179&imgtype=0&src=http%3A%2F%2Fs9.sinaimg.cn%2Fmw690%2F006bYgeozy7pHERNHmg18%26690",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547976453462&di=b3aeda0529188a8aacddc751c6cd06b6&imgtype=0&src=http%3A%2F%2Fs11.sinaimg.cn%2Fmw690%2F006bYgeozy7pHES81p87a%26690", nil];
     [section0 addItem:it];
     
     QLHomeCategoryItem *itCategory = [[QLHomeCategoryItem alloc] init];
